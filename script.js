ad8a4eef2ee4a3e28568357e0cc06ae7e565d770
Vishalk4kumar <s222342946@deakin.edu.au>
2023-08-01 19:03:56 +1000

    commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const cardList = [
+    {
+        title: "Kitty 2",
+        image: "images/kitty2.jpg",
+        link: "About Cat 2",
+        desciption: "Demo desciption about Cat 2"
+    },
+    {
+        title: "Kitty 3",
+        image: "images/kitty3.jpg",
+        link: "About Cat 3",
+        desciption: "Demo desciption about Cat 3"
+    },
+    {
+        title: "Kitty 4",
+        image: "images/kitty4.jpg",
+        link: "About Cat 4",
+        desciption: "Demo desciption about Cat 4"
+    }
+]
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+    addCards(cardList);
+    $('.modal').modal();
+  });
